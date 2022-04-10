71246b0871a98dfd464ca64e89ac66fffd605f9d
Jakkaphat <gunt@guntxjakka.me>
2022-04-11 04:31:49 +0700

    build: add file name flag

--- build.py
+++ build.py
@@ -1,11 +1,17 @@
 import PyInstaller.__main__
+import toml
+import platform
 
 
 def main() -> None:
+    with open("pyproject.toml") as f:
+        file = toml.load(f)
+    ver = str(file['tool']['poetry']['version'])
     PyInstaller.__main__.run([
         'main.py',
         '--onefile',
-        '--console'
+        '--console',
+        f'--name=numbguess-{ver}-{platform.uname().system}'
     ])
 
 
