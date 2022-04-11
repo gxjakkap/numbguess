5f97e05e48254fae4e368a42653a2d19457c483b
Jakkaphat <gunt@guntxjakka.me>
2022-04-12 00:45:03 +0700

    build: all lower os name

--- build.py
+++ build.py
@@ -11,7 +11,7 @@ def main() -> None:
         'main.py',
         '--onefile',
         '--console',
-        f'--name=numbguess-{ver}-{platform.uname().system}',
+        f'--name=numbguess-{ver}-{platform.uname().system.lower()}',
         '-p /gamemode/*.py'
     ])
 
